d290cc6499401d9b7ce68a6c8feedc4e4dc675dd
Airon Valentim <aironvalentim@gmail.com>
2025-03-30 12:00:08 -0300

    Adicionando docstring na função salvarCSV

--- src/load.py
+++ src/load.py
@@ -1,5 +1,19 @@
 import pandas as pd 
 
 def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str):
+    
+    """
+    Função para salvar um DataFrame em um arquivo CSV.
+
+    Parâmetros:
+    df (pd.DataFrame): O DataFrame que será salvo.
+    nome_arquivo (str): O nome do arquivo CSV a ser criado.
+    separador (str): O caractere usado como separador de colunas no arquivo CSV.
+    decimal (str): O caractere usado para separar os valores decimais no arquivo CSV.
+
+    Retorna:
+    None: A função apenas salva o arquivo e não retorna nenhum valor.
+    """
+    
     df.to_csv(nome_arquivo, sep=separador, decimal=decimal)
     return
